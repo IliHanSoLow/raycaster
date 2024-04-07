75d0b566d50b5e75333cfc56b6f4d8d7cfb33355
IliHanSoLow <ilian-ode@gmx.de>
2024-04-07 18:06:47 +0200

    tried to draw 100 squares

--- src/main.c
+++ src/main.c
@@ -5,27 +5,173 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_GLFW_GL3_IMPLEMENTATION
-#define NK_KEYSTATE_BASED_INPUT
-#include <nuklear.h>
-#include <nuklear_glfw_gl3.h>
-
 GLFWwindow *window;
-
-struct nk_glfw nkGLFW;
-struct nk_context *nkContext;
 
 GLuint vbo, ibo, vao, program;
 
 float px, py;
+
+GLuint map_vbo, map_ibo, map_vao, map_program;
+GLfloat map_verticees[400];
+GLint map_indecies[600];
+
+int map[10][10] = {
+    //////////////////////////////////
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, //
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //
+    {1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, //
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //
+    {1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //
+    {1, 0, 0, 1, 0, 0, 1, 0, 0, 1}, //
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, //
+};
+
+void setMapVertsInds() {
+  int innner_size = sizeof(map[0]) / sizeof(int);
+  int outer_size = sizeof(map) / innner_size / sizeof(int);
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      float a = (j - 10 + 1) / 11.f;        //(2,3) 0.1
+      float b = (j - 10 - 0.9f + 1) / 11.f; //      0.14
+      float c = (i + 1) / 11.f;             //      0.15
+      float d = (i - 0.09f + 1) / 11.f;     //      0.19
+
+      printf("%f,%f\n", a, c);
+      printf("%f,%f\n", b, c);
+      printf("%f,%f\n", a, d);
+      printf("%f,%f\n", b, d);
+
+      map_verticees[i * j + 0] = (float)a; // / 100.0f
+      map_verticees[i * j + 1] = (float)c; // / 100.0f
+                                           // /
+      map_verticees[i * j + 2] = (float)b; // / 100.0f
+      map_verticees[i * j + 3] = (float)c; // / 100.0f
+                                           // /
+      map_verticees[i * j + 4] = (float)a; // / 100.0f
+      map_verticees[i * j + 5] = (float)d; // / 100.0f
+                                           // /
+      map_verticees[i * j + 6] = (float)b; // / 100.0f
+      map_verticees[i * j + 7] = (float)d; // / 100.0f
+
+      map_indecies[i * j + 0] = i * j + 0;
+      map_indecies[i * j + 1] = i * j + 1;
+      map_indecies[i * j + 2] = i * j + 2;
+      map_indecies[i * j + 3] = i * j + 1;
+      map_indecies[i * j + 4] = i * j + 2;
+      map_indecies[i * j + 5] = i * j + 3;
+    }
+  }
+}
+
+void getMapVerts() {
+  int innner_size = sizeof(map[0]) / sizeof(int);
+  int outer_size = sizeof(map) / innner_size / sizeof(int);
+  printf("[\n");
+  printf("(");
+  for (int i = 0; i < outer_size * innner_size; i++) {
+    printf("%f", map_verticees[i]);
+    if ((i + 1) % 8 == 0 && i != 0) {
+      printf("),\n(");
+    } else if ((i + 1) % 2 == 0) {
+      printf("), (");
+    } else {
+      printf("|");
+    }
+  }
+  printf("\n]");
+  printf("%d", outer_size * innner_size);
+}
+
+void setMapProgram() {
+  const char *vsCode = ""
+                       "#version 430\n"
+                       "layout(location = 0) in vec4 Position;\n"
+                       "void main()\n"
+                       "{\n"
+                       "	gl_Position = Position;\n"
+                       "}\n";
+
+  const char *fsCode = ""
+                       "#version 430\n"
+                       "layout(location = 0) out vec4 Color;\n"
+                       "void main()\n"
+                       "{\n"
+                       "	Color = vec4(1.0, 1.0, 1.0, 1.0);\n"
+                       "}\n";
+
+  glGenVertexArrays(1, &map_vao);
+  glBindVertexArray(map_vao);
+
+  glGenBuffers(1, &map_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, map_vbo);
+
+  glGenBuffers(1, &map_ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, map_ibo);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(map_verticees), map_verticees,
+               GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(map_indecies), map_indecies,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  map_program = glCreateProgram();
+  GLuint status;
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vsCode, NULL);
+  glCompileShader(vs);
+  glGetShaderiv(vs, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    GLsizei messageLength;
+    GLchar message[256];
+    glGetShaderInfoLog(vs, 256, &messageLength, message);
+    fprintf(stderr, "[OpenGL] Failed to compile vertex shader: \n%s\n",
+            message);
+  }
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fsCode, NULL);
+  glCompileShader(fs);
+  glGetShaderiv(fs, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    GLsizei messageLength;
+    GLchar message[256];
+    glGetShaderInfoLog(fs, 256, &messageLength, message);
+    fprintf(stderr, "[OpenGL] Failed to compile fragment shader: \n%s\n",
+            message);
+  }
+
+  glAttachShader(map_program, vs);
+  glAttachShader(map_program, fs);
+
+  glLinkProgram(map_program);
+  glGetProgramiv(map_program, GL_LINK_STATUS, &status);
+  if (status != GL_TRUE) {
+    GLsizei messageLength;
+    GLchar message[256];
+    glGetProgramInfoLog(fs, 256, &messageLength, message);
+    fprintf(stderr, "[OpenGL] Failed to link program: \n%s\n", message);
+  }
+  glValidateProgram(map_program);
+  glGetProgramiv(program, GL_VALIDATE_STATUS, &status);
+  if (status != GL_TRUE) {
+    GLsizei messageLength;
+    GLchar message[256];
+    glGetProgramInfoLog(fs, 256, &messageLength, message);
+    fprintf(stderr, "[OpenGL] Failed to validate program: \n%s\n", message);
+  }
+  glDeleteShader(fs);
+  glDeleteShader(vs);
+}
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -128,13 +274,19 @@ void onGraphicsCleanup() {
   glDeleteVertexArrays(1, &vao);
 
   glDeleteProgram(program);
+
+  glDeleteBuffers(1, &map_vbo);
+  glDeleteBuffers(1, &map_ibo);
+  glDeleteVertexArrays(1, &map_vao);
+  glDeleteProgram(map_program);
 }
 
 /*
  * Runs when the game starts
  */
 void onInit() {
-  px = 300;
+  setMapVertsInds();
+  px = 100;
   py = 300;
 }
 
@@ -159,10 +311,11 @@ void onUpdate() {
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glUseProgram(program);
-  glBindVertexArray(vao);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+  // Draw map
+  glUseProgram(map_program);
+  glBindVertexArray(map_vao);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, map_ibo);
+  glDrawElements(GL_TRIANGLES, 600, GL_UNSIGNED_INT, NULL);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
   glUseProgram(0);
   glBindVertexArray(0);
@@ -290,6 +443,9 @@ void glDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
 }
 
 int main() {
+  /*
+   * Setup OpenGL
+   */
   glfwSetErrorCallback(glfwErrorCallback);
 
   if (!glfwInit()) {
@@ -297,6 +453,9 @@ int main() {
     return -1;
   }
 
+  /*
+   * Create Window
+   */
   glfwDefaultWindowHints();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -323,56 +482,70 @@ int main() {
     return -1;
   }
 
-  nkContext = nk_glfw3_init(&nkGLFW, window, NK_GLFW3_INSTALL_CALLBACKS);
-  {
-    struct nk_font_atlas *atlas;
-    nk_glfw3_font_stash_begin(&nkGLFW, &atlas);
-    /*struct nk_font *droid = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/DroidSans.ttf", 14, 0);*/
-    /*struct nk_font *roboto = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/Roboto-Regular.ttf", 14, 0);*/
-    /*struct nk_font *future = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/kenvector_future_thin.ttf", 13, 0);*/
-    /*struct nk_font *clean = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/ProggyClean.ttf", 12, 0);*/
-    /*struct nk_font *tiny = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/ProggyTiny.ttf", 10, 0);*/
-    /*struct nk_font *cousine = nk_font_atlas_add_from_file(atlas,
-     * "../../../extra_font/Cousine-Regular.ttf", 13, 0);*/
-    nk_glfw3_font_stash_end(&nkGLFW);
-    /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
-    /*nk_style_set_font(ctx, &droid->handle);*/
-  }
-
   glEnable(GL_DEBUG_OUTPUT);
   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
   glDebugMessageCallback(glDebugCallback, NULL);
 
+  /*
+   * Game Structure
+   */
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
   onGraphicsInit();
   onInit();
   while (onFrame()) {
-    nk_glfw3_new_frame(&nkGLFW);
-
-    if (nk_begin(nkContext, "Test", nk_rect(50, 50, 230, 250),
-                 NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
-                     NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE)) {
-    }
-    nk_end(nkContext);
 
     onUpdate();
     onInput();
-
-    nk_glfw3_render(&nkGLFW, NK_ANTI_ALIASING_ON, 512 * 1024, 128 * 1024);
   }
   onExit();
   onGraphicsCleanup();
-
-  nk_glfw3_shutdown(&nkGLFW);
 
   glfwDestroyWindow(window);
   glfwTerminate();
 
   return 0;
+}
+
+void drawPlayer() {
+  GLfloat vertices[] = {px + 2, py, px, py + 2, px, py - 2};
+  GLuint indices[] = {0, 1, 2, 2, 3, 0};
+
+  const char *vsCode = ""
+                       "#version 430\n"
+                       "layout(location = 0) in vec4 Position;\n"
+                       "void main()\n"
+                       "{\n"
+                       "	gl_Position = Position;\n"
+                       "}\n";
+
+  const char *fsCode = ""
+                       "#version 430\n"
+                       "layout(location = 0) out vec4 Color;\n"
+                       "void main()\n"
+                       "{\n"
+                       "	Color = vec4(1.0, 1.0, 1.0, 1.0);\n"
+                       "}\n";
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  program = glCreateProgram();
 }
